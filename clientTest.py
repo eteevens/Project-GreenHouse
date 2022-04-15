795e548c004d37b961014e487ccf899181cb82f9
eteevens <teevense@gmail.com>
2022-04-15 18:59:25 -0400

    synchronization between graph and data client now handled with flag

--- clientTest.py
+++ clientTest.py
@@ -14,7 +14,7 @@ url = "http://127.0.0.1:5000" #address of the server
 write_to_app = url + "/read_client" #server page for read
 read_from_app = url + "/write_client" #server page for write
 
-checkRateGraph = 0.5 #how often to check the graph, NEEDS to be same as server
+checkRateGraph = 0.5 #how often to write to the graph (temp value)
 
 def send_and_recieve():
 
@@ -24,6 +24,10 @@ def send_and_recieve():
         value1 = (value1 + 15) % 100
         value2 = int(np.random.rand() * 100) #random fake data
         value3 = int(np.random.rand() * 100)
+        value4 = 95
+        value5 = 10
+        value6 = 85
+        value7 = 50
 
         data_to_send = {'time':datetime.now().strftime('%H:%M:%S')}
         #adds the time to the json to be sent, this MUST be sent because
@@ -32,6 +36,10 @@ def send_and_recieve():
         data_to_send['Temperature (in F)'] = value1 #name of the data (to be displayed) : value
         data_to_send['Humidity'] = value2
         data_to_send['Light Level'] = value3
+        data_to_send['Max Temperature Set Point'] = value4
+        data_to_send['Min Temperature Set Point'] = value5
+        data_to_send['Max Humidity Set Point'] = value6
+        data_to_send['Day/Night Line'] = value7
 
         json_data = json.dumps(data_to_send) #turns the data into a JSON string
 
@@ -40,7 +48,7 @@ def send_and_recieve():
 
         print(r.json()) #print out what is read
 
-        time.sleep(checkRateGraph) #wait (for graph sync)
-
+        time.sleep(checkRateGraph) #wait
+        
 if __name__ == "__main__":
     send_and_recieve()
